52bc7b28efb34a92d4010249efcef14e7f97b772
roromaniv <37473825+roromaniv@users.noreply.github.com>
2021-07-25 02:54:41 +0300

    Update submit.js

--- submit.js
+++ submit.js
@@ -1,12 +1,6 @@
 let submit = function(){
-    if(document.getElementsByName("email")[0].checkValidity()){
-        let values = [document.getElementsByName("name")[0].value,document.getElementsByName("message")[0].value,document.getElementsByName("email")[0].value];
-        let req = "https://maker.ifttt.com/trigger/contact/with/key/KjsjUkae4gJm-W8xYRIyK?value1=" + escape(values[0]) + "&value2=" + escape(values[1]) + "&value3=" + escape(values[2]);
-        let embed = "<iframe style='display:none; width:0px; height:0px;' onload='this.remove()' src='" + req + "'></iframe>";
-        document.getElementById("contactform").insertAdjacentHTML("afterend",embed);
-    }
-    else
-    {
-        document.getElementsByName("email")[0].checkValidity();
-    }
+    let values = [document.getElementsByName("name")[0].value,document.getElementsByName("message")[0].value,document.getElementsByName("email")[0].value];
+    let req = "https://maker.ifttt.com/trigger/contact/with/key/KjsjUkae4gJm-W8xYRIyK?value1=" + escape(values[0]) + "&value2=" + escape(values[1]) + "&value3=" + escape(values[2]);
+    let embed = "<iframe style='display:none; width:0px; height:0px;' onload='this.remove()' src='" + req + "'></iframe>";
+    document.getElementById("contactform").insertAdjacentHTML("afterend",embed);
 }
