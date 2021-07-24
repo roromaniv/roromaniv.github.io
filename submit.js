4329218bca57301f1500c0d517c67a41cfd579e5
roromaniv <37473825+roromaniv@users.noreply.github.com>
2021-07-25 01:49:09 +0300

    Update submit.js

--- submit.js
+++ submit.js
@@ -1,6 +1,6 @@
 let submit = function(){
     alert("submit");
-    let values = [document.getElementsByName("name").value,document.getElementsByName("email").value,document.getElementsByName("message").value];
+    let values = [document.getElementsByName("name")[0].value,document.getElementsByName("email")[0].value,document.getElementsByName("message")[0].value];
     let req = "https://maker.ifttt.com/trigger/contact/with/key/KjsjUkae4gJm-W8xYRIyK?value1=" + escape(values[0]) + "&value2=" + escape(values[1]) + "&value3=" + escape(values[2]);
     let embed = "<iframe src='" + req + "'></iframe>";
     alert("success");
