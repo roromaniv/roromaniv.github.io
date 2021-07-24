3cd8d76fbf55832a01924111c40c5209fc21d494
roromaniv <37473825+roromaniv@users.noreply.github.com>
2021-07-25 01:14:29 +0300

    rewired form

---
+++ submit.js
@@ -0,0 +1,7 @@
+let submit = function(){
+    let values = [document.getElementsByName("name"),document.getElementsByName("email"),document.getElementsByName("message")];
+    document.getElementById("contact").innerHTML+="<iframe src=" + embed + "></iframe>"
+
+    let embed = "https://maker.ifttt.com/trigger/contact/with/key/KjsjUkae4gJm-W8xYRIyK?value1=" + escape(values[0]) + "&value2=" + escape(values[1]) + "&value3=" + escape(values[2]);
+    
+}
