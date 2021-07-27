c94d1d10099e4fecdb46d8a18e80d265afbd6c1a
roromaniv <37473825+roromaniv@users.noreply.github.com>
2021-07-27 22:17:41 +0300

    Update contact.js

--- contact.js
+++ contact.js
@@ -22,8 +22,14 @@ let envelope = function(){
 }
 
 let switchToRight = function(link){
-    document.getElementsByClassName("head")[0].animate([
-        { transform: 'translate(0, 0)', opacity: 1 },
-        { transform: 'translate(-50%, 0)', opacity: 0}], 2000);
+ //   document.getElementsByClassName("head")[0].animate([
+   //     { transform: 'translate(0, 0)', opacity: 1 },
+     //   { transform: 'translate(-50%, 0)', opacity: 0}], 2000);
+
+    document.getElementsByClassName("head")[0].animate({
+        transform: ["translate(0,0)","translate(-50%,0)"],
+        opacity: [1,0],
+        easing: ["ease-out"],
+    }, 2000);
 }
 
